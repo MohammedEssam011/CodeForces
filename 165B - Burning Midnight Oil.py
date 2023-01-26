c8a9565dcbdf0aecd653108f828681c2cbe16e6c
Mohammed Essam <60797888+MohammedEssam011@users.noreply.github.com>
2023-01-26 10:42:12 +0200

    Update 165B - Burning Midnight Oil.py

--- 165B - Burning Midnight Oil.py
+++ 165B - Burning Midnight Oil.py
@@ -1,1 +1,13 @@
-
+n, k =map(int,input().split())
+left, right = 1, n
+while left != right:
+    x = middle = (left + right) // 2
+    sum = 0
+    while x != 0:
+        sum += x
+        x //= k
+    if sum >= n:
+        right = middle
+    else:
+        left = middle + 1
+print(left)
